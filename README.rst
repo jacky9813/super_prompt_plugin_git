0620054a9519b6908310bf6d23fd0ff3e35a0612
Jacky Chen <jacky9813@hotmail.com>
2023-08-16 00:12:34 +0800

    initial commit

---
+++ README.rst
@@ -0,0 +1,34 @@
+#########################
+Super Prompt - Git Plugin
+#########################
+
+.. _Super Prompt: https://github.com/jacky9813/super_prompt
+
+A plugin for `Super Prompt`_ that indicates the current state of git repository.
+
+
+System Requirements
+===================
+
+- Python 3.7+
+- `Super Prompt`_
+- Git 1.7.0+
+
+
+Install
+=======
+
+Super Prompt - Git Plugin has not been published to PyPI yet.
+
+Thus, you can install from this Git repo:
+
+.. code-block:: shell
+
+    pip3 install git+https://github.com/jacky9813/super_prompt_plugin_git
+    super-prompt config enable-plugin git
+
+
+Configuration
+=============
+
+No configuration available for this plugin.
---
+++ super_prompt_plugin_git/main.py
@@ -0,0 +1,53 @@
+import typing
+import logging
+import os
+
+import git
+import git.exc
+
+import super_prompt.types
+
+GIT_SYMBOL = "\U0001f709"
+GIT_SYNCED = "\u2713 "
+GIT_COMMIT_NOT_PUSHED = "\u21c5"
+GIT_DIRTY = "*"
+
+logger = logging.getLogger("super_prompt_plugin_git")
+
+
+def main(config: dict) -> typing.Optional[super_prompt.types.PluginResponse]:
+    repo = None
+    cursor = os.getcwd()
+    while cursor:
+        try:
+            repo = git.Repo(cursor)
+            break
+        except git.exc.InvalidGitRepositoryError:
+            next_cursor = os.path.abspath(os.path.join(cursor, ".."))
+            if next_cursor == cursor:
+                cursor = None
+            else:
+                cursor = next_cursor
+    if repo is None:
+        logger.info("No valid Git repo found")
+        return
+    branch_name = repo.active_branch.name
+
+    if repo.is_dirty():
+        return super_prompt.types.PluginResponse(GIT_DIRTY + GIT_SYMBOL, branch_name, None)
+
+    remote_branch = repo.active_branch.tracking_branch()
+    if remote_branch is None:
+        return super_prompt.types.PluginResponse(GIT_SYMBOL, branch_name, None)
+    
+    return super_prompt.types.PluginResponse(
+        (GIT_SYNCED if remote_branch.commit.hexsha == repo.active_branch.commit else GIT_COMMIT_NOT_PUSHED) + GIT_SYMBOL,
+        branch_name,
+        None
+    )
+
+    
+
+if __name__ == "__main__":
+    print(main({}))
+
